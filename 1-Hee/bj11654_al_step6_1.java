63a90a96304d0388c027e0b7b980dfd85641a4d5
1-Hee <onehee9710@gmail.com>
2022-04-08 21:49:25 +0900

    [BOJ] 아스키코드 / 단계:6 / 5분
    https://www.acmicpc.net/problem/11654

---
+++ 1-Hee/bj11654_al_step6_1.java
@@ -0,0 +1,17 @@
+package bakjoon2;
+
+import java.util.Scanner;
+
+public class bj11654_al_step6_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String st1 = sc.nextLine();
+		char x = st1.charAt(0);		
+		System.out.println((int) x);
+		
+
+	}
+
+}
